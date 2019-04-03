1b2f637e27e44b02b186a1cf74da1211ff9d750a
sunhao <2080680175@qq.com>
2019-04-03 13:43:24 +0800

    委托

--- LearnSharp.CustomDelegate/Program.cs
+++ LearnSharp.CustomDelegate/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LearnSharp.CustomDelegate
+namespace LearnSharp.CustomDelegate   
 {
     //委托使用的演示
     class Program
@@ -14,16 +14,22 @@ namespace LearnSharp.CustomDelegate
 
         static void Main(string[] args)
         {   
-            //2、声明委托变量d
-            MyDelegate d;
+            ////2、声明委托变量d
+            //MyDelegate d;
 
-            //3实例化委托类型，传递的方法也可以为静态方法，这里传递的是静态方法
-            d = new MyDelegate(new Program().Add);
+            ////3实例化委托类型，传递的方法也可以为静态方法，这里传递的是静态方法
+            //d = new MyDelegate(new Program().Add);
 
-            //4、委托类型作为参数传递给另一个方法
-            MyMethod(d);
+            ////4、委托类型作为参数传递给另一个方法
+            //MyMethod(d);
+
+            Program p = new Program();
+            p.Greeting("李志", p.ChineseGreeting);
+            p.Greeting("Tommy Li", p.EnglishGreeting);
 
             Console.Read();
+            var y = Console.Read();
+
         }
 
         //该方法的定义必须与委托类型相同，即返回类型为void，两个int类型的参数
@@ -38,6 +44,29 @@ namespace LearnSharp.CustomDelegate
         private static void MyMethod(MyDelegate myDelegate)
         {
             myDelegate(1, 2);
+        }
+
+
+        //定义委托类型
+        public delegate int GreetingDelegate(string name);
+
+        //有了委托之后，可以像下面这样实现打招呼的方法
+        public void Greeting(string name, GreetingDelegate callback)
+        {
+            var ttt = callback.Invoke(name);
+            Console.WriteLine(ttt);
+        }
+
+        public int EnglishGreeting(string name)
+        {
+            Console.WriteLine("Hello," + name);
+            return 1;
+        }
+
+        public int ChineseGreeting(string name)
+        {
+            Console.WriteLine("你好，" + name);
+            return 2;
         }
     }
 }
