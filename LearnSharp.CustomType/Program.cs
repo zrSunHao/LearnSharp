6ef4157d655864d42bd7e276f6ec8a8ccc9757c8
sunhao <2080680175@qq.com>
2019-04-03 16:13:38 +0800

    类型

---
+++ LearnSharp.CustomType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSharp.CustomType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //值类型
+            int valueType = 3;
+
+            //引用类型
+            string reftype = "abc";
+        }
+    }
+}
