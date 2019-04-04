a16f767ed8e76a27b2313517c102132393e725e3
sunhao <2080680175@qq.com>
2019-04-04 09:50:58 +0800

    1

--- LearnSharp.CustomGeneric1/Program.cs
+++ LearnSharp.CustomGeneric1/Program.cs
@@ -10,6 +10,30 @@ namespace LearnSharp.CustomGeneric1
     {
         static void Main(string[] args)
         {
+            int n1 = 1;
+            int n2 = 2;
+
+            //不使用类型推断的代码
+            GenericMethod<int>(ref n1,ref n2);
+
+            //使用类型推断的代码
+            GenericMethod(ref n1, ref n2);
+            Console.WriteLine("n1的值现在为：" + n1);
+            Console.WriteLine("n2的值现在为：" + n2);
+
+            string str1 = "123";
+            object obj = "456";
+
+            //使用类型推断时可能会出现编译错误
+            //GenericMethod(ref str1,ref obj);
+            Console.Read();
+        }
+
+        private static void GenericMethod<T>(ref T t1, ref T t2)
+        {
+            T temp = t1;
+            t1 = t2;
+            t2 = temp;
         }
     }
 }
